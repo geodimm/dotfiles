01508bcadccaf8502d69fa1821f1a25852b135ef
Georgi Dimitrov <georgijd92@gmail.com>
2015-02-19 00:00:36 +0000

    Change string format for git branch in the prompt.
    Use square brackets instead of normal brackets, mainly for consistency
    with the rest of the prompt.

--- git-prompt.sh
+++ git-prompt.sh
@@ -292,7 +292,7 @@ __git_ps1 ()
 	local detached=no
 	local ps1pc_start='\u@\h:\w '
 	local ps1pc_end='\$ '
-	local printf_format=' (%s)'
+	local printf_format='[%s]'
 
 	case "$#" in
 		2|3)	pcmode=yes
