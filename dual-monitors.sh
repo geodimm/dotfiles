8766bd3d8d0b92e517bc68f1f4e45a7fb61bfd7f
Georgi Dimitrov <georgijd92@gmail.com>
2018-06-07 22:48:54 +0100

    Get rid of the dual monitors setup

--- dual-monitors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ `xrandr --listactivemonitors | head -n1 | cut -d' ' -f2` = "1" ]; then
-    xrandr \
-        --output eDP-1 --mode 1680x1050 --pos 0x390 --rotate normal --primary
-else
-    xrandr \
-        --output eDP-1 --mode 1680x1050 --pos 0x390 --rotate normal \
-        --output HDMI-3 --primary --mode 2560x1440 --pos 1680x0 --rotate normal
-fi
