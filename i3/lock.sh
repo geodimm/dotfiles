95a7f035d888c41f626770ae1836e179ce35d364
Georgi Dimitrov <georgijd92@gmail.com>
2018-08-23 20:06:23 +0100

    Apply various random improvements

--- i3/lock.sh
+++ i3/lock.sh
@@ -7,11 +7,5 @@ tmpbg='/tmp/screen.png'
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 20% -scale 500% "$tmpbg"
-case $(xrandr -q | grep -Eo 'current [[:digit:]]+ x [[:digit:]]+' | sed 's/current //; s/ //g') in
-    4240x1440)
-        convert "$tmpbg" "$icon" -gravity center -geometry +840 -composite -matte "$tmpbg"
-        convert "$tmpbg" "$icon" -gravity center -geometry -1280+195 -composite -matte "$tmpbg";;
-    *)
-        convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg";;
-esac
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 exec i3lock -i "$tmpbg"
