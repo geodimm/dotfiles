2b20dfec5a69a8a7ab0f25469209f4632d6e56e8
Georgi Dimitrov <georgijd92@gmail.com>
2019-06-07 11:09:28 +0100

    Switch to zsh

--- tmux/tnew.sh
+++ tmux/tnew.sh
@@ -5,7 +5,7 @@ SESSION_NAME="work"
 if ! tmux has-session -t ${SESSION_NAME}
 then
     tmux new-session -s ${SESSION_NAME} -n "deploy" -d
-    tmux new-window -n "bash" -t ${SESSION_NAME}
+    tmux new-window -n "${SHELL}" -t ${SESSION_NAME}
     tmux send-keys -t ${SESSION_NAME}:1 'cd /repos/env/deploy && gu' C-m
     tmux send-keys -t ${SESSION_NAME}:2 'cd /repos' C-m
     tmux -2 attach-session -t ${SESSION_NAME}
