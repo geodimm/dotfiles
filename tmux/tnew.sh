1cd310d69f5a54772b5f8f8d27b252ffb00b726e
Georgi Dimitrov <georgijd92@gmail.com>
2019-10-28 17:55:40 +0000

    Update the default tmux session windows

--- tmux/tnew.sh
+++ tmux/tnew.sh
@@ -4,9 +4,9 @@ SESSION_NAME="work"
 
 if ! tmux has-session -t ${SESSION_NAME}
 then
-    tmux new-session -s ${SESSION_NAME} -n "deploy" -d
+    tmux new-session -s ${SESSION_NAME} -n "cmd" -d
     tmux new-window -n "repos" -t ${SESSION_NAME}
-    tmux send-keys -t ${SESSION_NAME}:1 'cd /repos/env/deploy && gu' C-m
+    tmux send-keys -t ${SESSION_NAME}:1 'cd /repos' C-m
     tmux send-keys -t ${SESSION_NAME}:2 'cd /repos' C-m
     tmux -2 attach-session -t ${SESSION_NAME}
 fi
