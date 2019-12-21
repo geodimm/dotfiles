d96fbf69419c550bd86d0fc51f725df19cac01d3
Georgi Dimitrov <georgijd92@gmail.com>
2019-12-21 12:51:51 +0000

    Update tmux bootstrap script

--- tmux/tnew.sh
+++ tmux/tnew.sh
@@ -2,11 +2,13 @@
 
 SESSION_NAME="work"
 
-if ! tmux has-session -t ${SESSION_NAME}
+if tmux has-session -t ${SESSION_NAME}
 then
-    tmux new-session -s ${SESSION_NAME} -n "cmd" -d
+    tmux -2 attach-session -t ${SESSION_NAME}
+else
+    tmux new-session -s ${SESSION_NAME} -n "notes" -d
     tmux new-window -n "repos" -t ${SESSION_NAME}
-    tmux send-keys -t ${SESSION_NAME}:1 'cd /repos' C-m
+    tmux send-keys -t ${SESSION_NAME}:1 'cd ~/vimwiki && vim' C-m
     tmux send-keys -t ${SESSION_NAME}:2 'cd /repos' C-m
     tmux -2 attach-session -t ${SESSION_NAME}
 fi
