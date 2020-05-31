a73638790bcbc31e86abb58ef271d532800b4ce2
Georgi Dimitrov <georgijd92@gmail.com>
2020-05-31 19:38:00 +0100

    Update the prompt command execution time thresholds

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -463,9 +463,9 @@
 
   ###################[ command_execution_time: duration of the last command ]###################
   # Show duration of the last command if takes longer than this many seconds.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=0
   # Show this many fractional digits. Zero means round to seconds.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=0
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=3
   # Execution time color.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND='#928374' # gray
   # Duration format: 1d 2h 3m 4s.
