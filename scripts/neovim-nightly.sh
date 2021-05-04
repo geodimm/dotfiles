b7bc0f8b5e39efcf45e213f04d749668a4dddbc2
Georgi Dimitrov <georgijd92@gmail.com>
2021-05-04 09:10:01 +0300

    Remove accidentally checked in line from neovim-nightly.sh

--- scripts/neovim-nightly.sh
+++ scripts/neovim-nightly.sh
@@ -8,7 +8,6 @@ do_install() {
     info "[neovim-nightly] Install"
     asset=$(curl --silent https://api.github.com/repos/neovim/neovim/releases/tags/nightly | jq -r '.assets // [] | .[] | select(.name | endswith("nvim.appimage")) | .url')
     if [[ -z $asset ]]; then warn "Cannot find a nightly release. Please try again later."; exit 0; fi;
-    return
     mkdir -p ~/bin
     curl --silent --location -H "Accept: application/octet-stream" "${asset}" > ~/bin/nvim
     chmod +x ~/bin/nvim
