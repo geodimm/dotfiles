2efb342e5f2fb6f45c91b8f6e1a0d34d229dc168
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-15 13:30:08 +0100

    Fix bufferline mappings

--- nvim/plugin/nvim-bufferline.lua
+++ nvim/plugin/nvim-bufferline.lua
@@ -12,7 +12,7 @@ require'bufferline'.setup {
 
 -- These commands will navigate through buffers in order regardless of which mode you are using
 --  e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
-vim.api.nvim_set_keymap('n', '<silent>[b', ':BufferLineCyclePrev<CR>',
-                        {noremap = true})
-vim.api.nvim_set_keymap('n', '<silent>]b', ':BufferLineCycleNext<CR>',
-                        {noremap = true})
+vim.api.nvim_set_keymap('n', '[b', ':BufferLineCyclePrev<CR>',
+                        {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', ']b', ':BufferLineCycleNext<CR>',
+                        {noremap = true, silent = true})
