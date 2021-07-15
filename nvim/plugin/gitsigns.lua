ec4aa8050c5d5baf977f44bd50ff88862e6413ba
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-15 10:38:56 +0100

    Update gitsigns highlight groups and text

--- nvim/plugin/gitsigns.lua
+++ nvim/plugin/gitsigns.lua
@@ -1,4 +1,11 @@
 require('gitsigns').setup {
+    signs = {
+        add = {hl = 'GitGutterAdd', text = '▌'},
+        change = {hl = 'GitGutterChange', text = '▌'},
+        delete = {hl = 'GitGutterDelete', text = '▌'},
+        topdelete = {hl = 'GitGutterDelete', text = '▌'},
+        changedelete = {hl = 'GitGutterChange', text = '▌'}
+    },
     current_line_blame = true,
     current_line_blame_delay = 0
 }
