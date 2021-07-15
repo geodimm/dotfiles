e40e9ab86c2745fa940ca4ccf3aaf571ad1e80ae
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-15 13:29:56 +0100

    Update bufferline diagnostics_indicator

--- nvim/plugin/nvim-bufferline.lua
+++ nvim/plugin/nvim-bufferline.lua
@@ -1,5 +1,13 @@
+local lsp_icons = {hint = "", info = "", warning = "", error = ""}
+
 require'bufferline'.setup {
-    options = {diagnostics = 'nvim_lsp', max_name_length = 20}
+    options = {
+        diagnostics = 'nvim_lsp',
+        max_name_length = 20,
+        diagnostics_indicator = function(count, level)
+            return " " .. lsp_icons[level] .. " " .. count
+        end
+    }
 }
 
 -- These commands will navigate through buffers in order regardless of which mode you are using
