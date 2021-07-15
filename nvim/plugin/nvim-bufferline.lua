8cc5f1193043989bfd559e342cf08dc8888747e1
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-15 12:48:02 +0100

    Enabled diagnostics in bufferline

--- nvim/plugin/nvim-bufferline.lua
+++ nvim/plugin/nvim-bufferline.lua
@@ -1,4 +1,6 @@
-require'bufferline'.setup {}
+require'bufferline'.setup {
+    options = {diagnostics = 'nvim_lsp', max_name_length = 20}
+}
 
 -- These commands will navigate through buffers in order regardless of which mode you are using
 --  e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
