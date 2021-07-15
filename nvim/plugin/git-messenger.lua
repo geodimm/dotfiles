62a9b0fb7901c11c8f983ad0092911ba22d9c218
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-15 14:56:29 +0100

    Register mappings in which-key

--- nvim/plugin/git-messenger.lua
+++ nvim/plugin/git-messenger.lua
@@ -1,3 +1,5 @@
 vim.g.git_messenger_include_diff = 'current'
 vim.g.git_messenger_floating_win_opts = {border = 'single'}
 vim.g.git_messenger_always_into_popup = true
+
+require("which-key").register({["<leader>gm"] = {"Show git blame"}})
