bebe9ad8f8967d43da471210c0d29ffb499ccd98
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-16 11:04:18 +0100

    Customize git line blame virtual text

--- nvim/plugin/gitsigns.lua
+++ nvim/plugin/gitsigns.lua
@@ -7,7 +7,20 @@ require('gitsigns').setup {
         changedelete = {hl = 'GitGutterChange', text = '▌'}
     },
     current_line_blame = true,
-    current_line_blame_delay = 0
+    current_line_blame_delay = 0,
+    current_line_blame_formatter = function(name, blame_info)
+        if blame_info.author == name then blame_info.author = 'You' end
+        local text
+        if blame_info.author == 'Not Committed Yet' then
+            text = blame_info.author
+        else
+            text = string.format('%s, %s - %s', blame_info.author, os.date(
+                                     '%Y-%m-%d',
+                                     tonumber(blame_info['author_time'])),
+                                 blame_info.summary)
+        end
+        return {{'   ' .. text, 'GitSignsCurrentLineBlame'}}
+    end
 }
 
 local wk = require("which-key")
