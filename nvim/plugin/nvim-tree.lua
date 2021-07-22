a74ceee50767adee5102a3ab88231b60f031ecbf
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-22 13:27:26 +0100

    Fix missing nvim-tree icons
    Something to do with lazy loading

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -1,8 +1,3 @@
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-local lib = require "nvim-tree.lib"
-
-function NvimTreeCloseParent() lib.parent_node(lib.get_node_at_cursor(), true) end
-
 vim.g.nvim_tree_auto_close = 0
 vim.g.nvim_tree_disable_netrw = 0
 vim.g.nvim_tree_hijack_netrw = 0
@@ -13,10 +8,14 @@ vim.g.nvim_tree_width = 50
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_bindings = {
-    {key = {"<CR>", "l", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")},
-    {key = "h", cb = ':lua NvimTreeCloseParent()<CR>'}
+    {
+        key = {"<CR>", "l", "o", "<2-LeftMouse>"},
+        cb = ":lua require('nvim-tree').on_keypress('edit')<CR>"
+    }, {
+        key = "h",
+        cb = ":lua lib = require('nvim-tree.lib');lib.parent_node(lib.get_node_at_cursor(), true)<CR>"
+    }
 }
-
 vim.api.nvim_set_keymap('n', '<leader>fe', ':NvimTreeToggle<CR>',
                         {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fl', ':NvimTreeFindFile<CR>',
