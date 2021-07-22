d88d30bb9876bfcb6839050eacfd1df3152e46db
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-22 14:59:47 +0100

    Update lsp icon usage and set nvim-tree icons

---
+++ nvim/lua/config/icons.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+local lsp_icons = {
+    hint = "",
+    info = "",
+    warning = "",
+    error = "",
+    other = ""
+}
+
+M.lsp = lsp_icons
+M.lspconfig = {
+    Hint = lsp_icons.hint,
+    Information = lsp_icons.info,
+    Warning = lsp_icons.warning,
+    Error = lsp_icons.error
+}
+
+return M
