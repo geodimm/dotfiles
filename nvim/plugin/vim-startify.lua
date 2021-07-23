56bbce39a02bcab7e4d61485766a3eb1dcfa8629
Georgi Dimitrov <georgijd92@gmail.com>
2021-07-23 13:05:29 +0100

    Update vim startify lists

--- nvim/plugin/vim-startify.lua
+++ nvim/plugin/vim-startify.lua
@@ -5,12 +5,9 @@ vim.g.startify_session_persistence = 1
 vim.g.startify_enable_special = 1
 
 vim.g.startify_lists = {
-    {type = 'sessions', header = {'   Sessions'}},
-    {type = 'files', header = {'   Files'}},
-    {
-        type = 'dir',
-        header = {'   Current Directory (' .. vim.fn.getcwd() .. ')'}
-    }, {type = 'bookmarks', header = {'   Bookmarks'}}
+    {type = 'sessions', header = {'     Sessions'}},
+    {type = 'files', header = {'     Files'}},
+    {type = 'bookmarks', header = {'     Bookmarks'}}
 }
 
 vim.g.startify_bookmarks = {
