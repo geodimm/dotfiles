cc7a2891185723c7c52c56c0b87f34eaaf574a27
Georgi Dimitrov <georgijd92@gmail.com>
2022-02-02 11:10:54 +0000

    Update gitsigns configuration

--- nvim/plugin/gitsigns.lua
+++ nvim/plugin/gitsigns.lua
@@ -21,6 +21,35 @@ require('gitsigns').setup({
                                  blame_info.summary)
         end
         return {{'   ' .. text, 'GitSignsCurrentLineBlame'}}
+    end,
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'",
+            {expr = true})
+        map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'",
+            {expr = true})
+
+        -- Actions
+        map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+        map('n', '<leader>hS', gs.stage_buffer)
+        map('n', '<leader>hu', gs.undo_stage_hunk)
+        map('n', '<leader>hR', gs.reset_buffer)
+        map('n', '<leader>hp', gs.preview_hunk)
+        map('n', '<leader>hb', function() gs.blame_line {full = true} end)
+        map('n', '<leader>hd', gs.diffthis)
+        map('n', '<leader>htd', gs.toggle_deleted)
+
+        -- Text object
+        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
     end
 })
 
@@ -34,10 +63,12 @@ wk.register({
         S = "Stage buffer",
         U = "Reset buffer index",
         b = "Blame line",
+        d = "Diff this",
         p = "Preview hunk",
         r = "Reset hunk",
         s = "Stage hunk",
-        u = "Undo stage hunk"
+        u = "Undo stage hunk",
+        td = "Toggle deleted"
     }
 })
 wk.register({
