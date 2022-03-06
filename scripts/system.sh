89abf866b5b6fe52dd54214a33af72284b5bd0b6
Georgi Dimitrov <georgijd92@gmail.com>
2022-03-06 14:14:05 +0000

    Add system configuration
    Creating various directories, installing patched fonts, etc.

---
+++ scripts/system.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+
+# shellcheck source=../scripts/util.sh
+source "$(pwd)/scripts/util.sh"
+
+FONTS_DIR="$HOME/.local/share/fonts"
+
+do_configure() {
+	info "[system] Create /repos directory"
+	sudo install -d -m 0755 -o "${USER}" -g "${USER}" /repos
+
+	info "[system] Create Java workspace directory"
+	install -d -m 0755 -o "${USER}" -g "${USER}" "$HOME/workspace"
+
+	# Font: MesloLGL Nerd Font Mono
+	# https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/Meslo/L/Regular/complete
+	info "[system] Install patched fonts"
+	local install_dir="/tmp/nerd-fonts"
+	rm -rf "$install_dir" && mkdir -p "$install_dir"
+	git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts "$install_dir"
+	cd "$install_dir"
+	git sparse-checkout add patched-fonts/Meslo/L/Regular
+	find . -type f -name '*.ttf' ! -name '*Windows*' -exec cp "{}" "$FONTS_DIR" \;
+	sudo fc-cache -f -v
+}
+
+main() {
+	command=$1
+	case $command in
+	"configure")
+		shift
+		do_configure "$@"
+		;;
+	*)
+		error "$(basename "$0"): '$command' is not a valid command"
+		;;
+	esac
+}
+
+main "$@"
