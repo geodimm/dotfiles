7e8b413cdb316df6626cca760743dc0fb37ebcbb
Georgi Dimitrov <georgijd92@gmail.com>
2022-04-14 19:53:36 +0100

    Install all tree-sitter parsers

--- nvim/plugin/nvim-treesitter.lua
+++ nvim/plugin/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = 'maintained',
+  ensure_installed = 'all',
   highlight = { enable = true },
   incremental_selection = {
     enable = true,
