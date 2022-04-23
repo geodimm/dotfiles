f35779c9ee7359fffafd443ce622f7f13825c506
Georgi Dimitrov <georgijd92@gmail.com>
2022-04-23 18:16:07 +0100

    Fix neovim bootstrapping

--- nvim/plugin/git-messenger.lua
+++ nvim/plugin/git-messenger.lua
@@ -2,4 +2,9 @@ vim.g.git_messenger_include_diff = 'current'
 vim.g.git_messenger_floating_win_opts = { border = 'single' }
 vim.g.git_messenger_always_into_popup = true
 
-require('which-key').register({ ['<leader>gm'] = { 'Show git blame' } })
+local status_ok, wk = pcall(require, 'which-key')
+if not status_ok then
+  return
+end
+
+wk.register({ ['<leader>gm'] = { 'Show git blame' } })
