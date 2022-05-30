92943a8a674c7efeb1b3f37327dd82e7426bbec6
Georgi Dimitrov <georgijd92@gmail.com>
2022-05-30 23:57:34 +0100

    use curved borders

--- nvim/init.lua
+++ nvim/init.lua
@@ -130,7 +130,7 @@ packer.startup({
   config = {
     display = {
       open_fn = function()
-        return require('packer.util').float({ border = 'single' })
+        return require('packer.util').float({ border = 'rounded' })
       end,
     },
   },
