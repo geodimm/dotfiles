1bf6ec4bd24708a16d38738c16d07961a80f6095
Georgi Dimitrov <georgijd92@gmail.com>
2022-07-23 12:14:11 +0100

    neovim: update cmdline completion sources

--- nvim/plugin/nvim-cmp.lua
+++ nvim/plugin/nvim-cmp.lua
@@ -165,4 +165,20 @@ cmp.setup.filetype('gitcommit', {
   }),
 })
 
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' },
+  }, {
+    { name = 'cmdline' },
+  }),
+})
+
 cmp_git.setup()
