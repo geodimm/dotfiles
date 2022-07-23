54d6e23d4477bb52e85352601401ee8db8916bd8
Georgi Dimitrov <georgijd92@gmail.com>
2022-07-23 12:13:05 +0100

    neovim: Update input UI

--- nvim/plugin/dressing.lua
+++ nvim/plugin/dressing.lua
@@ -11,8 +11,14 @@ end
 dressing.setup({
   input = {
     winblend = 0,
+    insert_only = false,
+    prompt_align = 'center',
+    relative = 'editor',
+    prefer_width = 0.5,
   },
   select = {
     telescope = telescope_themes.get_cursor({}),
+    winblend = 0,
+    width = 0.5,
   },
 })
