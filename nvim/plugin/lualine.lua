b793e26ccfb6a4fa74bd273ca728278d92c92d1b
Georgi Dimitrov <georgijd92@gmail.com>
2022-07-23 13:57:27 +0100

    neovim: update lualine configuration

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -3,57 +3,91 @@ if not status_ok then
   return
 end
 
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+  return function(str)
+    local win_width = vim.fn.winwidth(0)
+    if hide_width and win_width < hide_width then
+      return ''
+    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+      return str:sub(1, trunc_len) .. (no_ellipsis and '' or '…')
+    end
+    return str
+  end
+end
+
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+
 lualine.setup({
   options = {
-    theme = require('config.theme').theme,
+    theme = 'auto',
     section_separators = { '', '' },
     component_separators = { '', '' },
     icons_enabled = true,
     globalstatus = true,
   },
+  extensions = { 'nvim-tree', 'fugitive', 'quickfix', 'toggleterm' },
   sections = {
     lualine_a = { { 'mode', upper = true } },
     lualine_b = {
       {
-        'diff',
-        right_padding = 0,
-        color_added = 'green',
-        color_modified = 'yellow',
-        color_removed = 'red',
-      },
-      {
-        'branch',
+        'b:gitsigns_head',
         icon = '',
-        format = function(s)
-          local result = string.sub(s, 0, 10)
-          if string.len(s) > 10 then
-            result = result .. '…'
-          end
-          return result
-        end,
+        fmt = trunc(100, 10, nil, false),
+        padding = { left = 1, right = 0 },
       },
+      { 'diff', source = diff_source },
     },
     lualine_c = {
-      { 'filename', file_status = true },
-      { 'diagnostics', sources = { 'nvim_diagnostic' }, padding = 0 },
+      {
+        'filename',
+        file_status = true,
+        color = function(_)
+          return vim.bo.modified and 'WarningMsg' or ''
+        end,
+        symbols = {
+          modified = ' ﱐ',
+          readonly = ' ',
+          unnamed = '[No name]',
+        },
+      },
+      { 'diagnostics', sources = { 'nvim_diagnostic' } },
     },
     lualine_x = { 'filetype' },
-    lualine_y = { { 'encoding', right_padding = 0 }, 'fileformat' },
+    lualine_y = { { 'encoding', padding = { left = 1, right = 0 } }, 'fileformat' },
     lualine_z = {
-      { 'progress', right_padding = 0 },
-      { 'location', left_padding = 0 },
+      { 'progress', padding = 0 },
+      { 'location', padding = { left = 0, right = 1 } },
     },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { { 'filename', file_status = true, path = 1 } },
-    lualine_x = { 'filetype' },
-    lualine_y = { { 'encoding', right_padding = 0 }, 'fileformat' },
-    lualine_z = {
-      { 'progress', right_padding = 0 },
-      { 'location', left_padding = 0 },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 1,
+        color = function(_)
+          return vim.bo.modified and 'WarningMsg' or ''
+        end,
+        symbols = {
+          modified = ' ﱐ',
+          readonly = ' ',
+          unnamed = '[No name]',
+        },
+      },
     },
+    lualine_x = { 'filetype' },
+    lualine_y = {},
+    lualine_z = {},
   },
-  extensions = { 'fzf', 'nvim-tree', 'fugitive', 'quickfix' },
 })
