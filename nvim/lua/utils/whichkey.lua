edecd4a684bd5709579f4154ea3086407e560c99
Georgi Dimitrov <georgijd92@gmail.com>
2022-07-26 00:26:53 +0100

    neovim: use nvim api to get hl group colours

--- nvim/lua/utils/whichkey.lua
+++ nvim/lua/utils/whichkey.lua
@@ -1,20 +1,5 @@
 local M = {}
 
-M.dump = function(o)
-  if type(o) == 'table' then
-    local s = '{ '
-    for k, v in pairs(o) do
-      if type(k) ~= 'number' then
-        k = '"' .. k .. '"'
-      end
-      s = s .. '[' .. k .. '] = ' .. M.dump(v) .. ','
-    end
-    return s .. '} '
-  else
-    return tostring(o)
-  end
-end
-
 local status_ok, wk
 status_ok, wk = pcall(require, 'which-key')
 
