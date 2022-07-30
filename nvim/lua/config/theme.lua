05374ed396de6422881f157423e09c7854c0886c
Georgi Dimitrov <georgijd92@gmail.com>
2022-07-30 17:10:41 +0100

    neovim: remove unused colorschemes

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -17,20 +17,9 @@ local setup = function()
   vim.go.t_8f = '[[38;2;%lu;%lu;%lum'
   vim.go.t_8b = '[[48;2;%lu;%lu;%lum'
   vim.opt.background = 'dark'
-  vim.g.onedark_terminal_italics = 1
-  vim.g.nord_bold = 1
-  vim.g.nord_italic = 1
-  vim.g.nord_italic_comments = 1
-  vim.g.nord_underline = 1
-  vim.g.nord_uniform_diff_background = 1
-  vim.g.gruvbox_italic = 1
-  vim.g.gruvbox_sign_column = 'bg0'
-  vim.g.disable_toggle_style = 1
   vim.g.tokyonight_dark_sidebar = false
   vim.g.tokyonight_dark_float = false
-  vim.g.vscode_style = 'dark'
-  vim.g.vscode_italic_comment = 1
-  vim.g.vscode_disable_nvimtree_bg = true
+  vim.g.tokyonight_lualine_bold = true
   pcall(vim.cmd, 'colorscheme ' .. theme)
 end
 
