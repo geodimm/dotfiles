15c0d383ef136ea59cb641b6987a0770ab8b6e9a
Georgi Dimitrov <georgijd92@gmail.com>
2022-07-30 17:11:41 +0100

    gnome-terminal: remove unused themes

--- scripts/gnome-terminal.sh
+++ scripts/gnome-terminal.sh
@@ -17,20 +17,8 @@ do_configure() {
 	rm -rf "$install_dir" && mkdir -p "$install_dir"
 	git clone --quiet https://github.com/Mayccoll/Gogh.git "$install_dir"
 
-	info "[gnome-terminal][configure][themes] One Dark"
-	"$install_dir/themes/one-dark.sh"
-
-	info "[gnome-terminal][configure][themes] Nord"
-	"$install_dir/themes/nord.sh"
-
-	info "[gnome-terminal][configure][themes] Gruvbox Dark"
-	"$install_dir/themes/gruvbox-dark.sh"
-
 	info "[gnome-terminal][configure][themes] Tokyo Night Storm"
 	"$install_dir/themes/tokyo-night-storm.sh"
-
-	info "[gnome-terminal][configure][themes] VS Code Dark+"
-	"$install_dir/themes/vs-code-dark-plus.sh"
 }
 
 main() {
