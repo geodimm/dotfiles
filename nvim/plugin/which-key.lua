d2f121b02ec07f331774c0ca91fbb433100021d7
Georgi Dimitrov <georgijd92@gmail.com>
2022-08-01 21:26:14 +0100

    neovim: define icons and key_labels for which-key

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -3,7 +3,22 @@ if not status_ok then
   return
 end
 
+local icons = require('user.icons')
+local key_labels = {
+  ['<space>'] = icons.keyboard.Space,
+  ['<cr>'] = icons.keyboard.Return,
+  ['<bs>'] = icons.keyboard.Backspace,
+  ['<tab>'] = icons.keyboard.Tab,
+}
+for k, v in pairs(key_labels) do
+  key_labels[k:upper()] = v
+end
+
 wk.setup({
   plugins = { spelling = { enabled = true } },
+  icons = {
+    group = icons.group .. ' ',
+  },
+  key_labels = key_labels,
   window = { border = 'rounded', margin = { 0, 0, 0, 0 }, padding = { 1, 1, 1, 1 } },
 })
