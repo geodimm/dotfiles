a1877415a1cb1d40c191bf4e9b373db80e7dce5a
Georgi Dimitrov <georgijd92@gmail.com>
2022-08-01 21:25:51 +0100

    neovim: add more icons

--- nvim/lua/user/icons.lua
+++ nvim/lua/user/icons.lua
@@ -1,5 +1,16 @@
 local M = {}
 
+local keyboard = {
+  Alt = 'הּ',
+  Backspace = '',
+  Caps = 'בּ',
+  Control = 'דּ',
+  Return = '',
+  Shift = 'וּ',
+  Space = '',
+  Tab = '',
+}
+
 local lsp = {
   error = '',
   hint = '',
@@ -8,7 +19,9 @@ local lsp = {
   warning = '',
 }
 
+M.group = ''
 M.lightbulb = ''
+M.keyboard = keyboard
 M.lsp = lsp
 M.lspconfig = {
   Error = lsp.error,
