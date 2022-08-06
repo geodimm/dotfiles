bf02c9b8dc39f61b646a308ff090554213657b01
Georgi Dimitrov <georgijd92@gmail.com>
2022-08-06 23:53:28 +0100

    fzf: add default options

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -4,6 +4,8 @@
 
 test -f "${HOME}/.fzf.zsh" && source "${HOME}/.fzf.zsh"
 
+FZF_DEFAULT_OPTS=--color=16,fg:7,bg:-1,hl:5,fg+:13,bg+:8,hl+:6,info:2,prompt:4,pointer:13,marker:3,spinner:4,header:4
+
 # fzf-tab settings
 
 # switch group using `,` and `.`
