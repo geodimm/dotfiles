7beacb3bd0516fe39017f785990ff41e7337ee42
Georgi Dimitrov <georgijd92@gmail.com>
2022-08-06 21:45:55 +0100

    zsh: update the fzf-tab colours

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -14,7 +14,7 @@ zstyle ':fzf-tab:*' prefix ''
 
 # use tmux popup (requires tmux 3.2)
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
-zstyle ':fzf-tab:*' fzf-flags --color=16,fg:7,bg:-1,hl:5,fg+:4,bg+:8,hl+:13,info:2,prompt:4,pointer:6,marker:3,spinner:4,header:4
+zstyle ':fzf-tab:*' fzf-flags --color=16,fg:7,bg:-1,hl:5,fg+:13,bg+:8,hl+:6,info:2,prompt:4,pointer:13,marker:3,spinner:4,header:4
 
 # Whether to automatically insert a space after the result.
 zstyle ':fzf-tab:*' insert-space false
