db1770006df1b9037a75ac434fd5686d8be3e1ed
Georgi Dimitrov <georgijd92@gmail.com>
2022-08-30 11:14:49 +0100

    neovim: update lspconfig UI

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -27,6 +27,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = '*',
   callback = function()
     vim.api.nvim_set_hl(0, 'PmenuThumb', { link = 'Visual' })
+    vim.api.nvim_set_hl(0, 'LspInfoBorder', { link = 'FloatBorder' })
   end,
 })
 
