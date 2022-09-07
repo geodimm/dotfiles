ecae17d15d93c1607a0fa77cbbcd41c57eaf7d2a
Georgi Dimitrov <georgijd92@gmail.com>
2022-09-07 12:33:00 +0100

    neovim: update colorscheme setup

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -2,11 +2,28 @@ local M = {}
 
 local name = 'tokyonight'
 
+local setup_tokyonight = function()
+  local status_ok, tokyonight = pcall(require, 'tokyonight')
+  if not status_ok then
+    return
+  end
+
+  tokyonight.setup({
+    style = 'storm',
+    transparent = false,
+    styles = {
+      sidebars = 'transparent',
+      floats = 'transparent',
+    },
+    lualine_bold = true,
+  })
+end
+
 local setup = function()
   vim.opt.background = 'dark'
-  vim.g.tokyonight_dark_sidebar = false
-  vim.g.tokyonight_dark_float = false
-  vim.g.tokyonight_lualine_bold = true
+  if name == 'tokyonight' then
+    setup_tokyonight()
+  end
   pcall(vim.cmd, 'colorscheme ' .. name)
 end
 
