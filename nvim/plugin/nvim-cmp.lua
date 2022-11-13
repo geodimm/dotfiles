268f0fe5bd3496c4be42f870321e203c101784c8
Georgi Dimitrov <georgijd92@gmail.com>
2022-11-13 12:47:19 +0000

    neovim: fix cmdline autocompletion

--- nvim/plugin/nvim-cmp.lua
+++ nvim/plugin/nvim-cmp.lua
@@ -234,9 +234,9 @@ cmp.setup.cmdline('/', {
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
-    { name = 'path' },
-  }, {
     { name = 'cmdline' },
+  }, {
+    { name = 'path' },
   }),
 })
 
