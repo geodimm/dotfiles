97abb350b2ad381fc49e60d797540d25cf100cae
Georgi Dimitrov <georgijd92@gmail.com>
2022-12-01 10:38:47 +0000

    fzf: disable separator in default options

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -4,7 +4,7 @@
 
 test -f "${HOME}/.fzf.zsh" && source "${HOME}/.fzf.zsh"
 
-FZF_DEFAULT_OPTS=--color=16,fg:7,bg:-1,hl:5,fg+:13,bg+:8,hl+:6,info:2,prompt:4,pointer:13,marker:3,spinner:4,header:4
+FZF_DEFAULT_OPTS="--color=16,fg:7,bg:-1,hl:5,fg+:13,bg+:8,hl+:6,info:2,prompt:4,pointer:13,marker:3,spinner:4,header:4 --no-separator"
 
 # fzf-tab settings
 
@@ -16,7 +16,7 @@ zstyle ':fzf-tab:*' prefix ''
 
 # use tmux popup (requires tmux 3.2)
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
-zstyle ':fzf-tab:*' fzf-flags --color=16,fg:7,bg:-1,hl:5,fg+:13,bg+:8,hl+:6,info:2,prompt:4,pointer:13,marker:3,spinner:4,header:4
+zstyle ':fzf-tab:*' fzf-flags --color=16,fg:7,bg:-1,hl:5,fg+:13,bg+:8,hl+:6,info:2,prompt:4,pointer:13,marker:3,spinner:4,header:4 --no-separator
 
 # Whether to automatically insert a space after the result.
 zstyle ':fzf-tab:*' insert-space false
