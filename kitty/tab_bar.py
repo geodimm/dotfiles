8676536971bad3f6c513788765e7e234ebd02d78
Georgi Dimitrov <georgijd92@gmail.com>
2023-02-24 12:31:29 +0000

    kitty: use a custom tab bar

---
+++ kitty/tab_bar.py
@@ -0,0 +1,60 @@
+import os
+from kitty.fast_data_types import Screen
+from kitty.tab_bar import DrawData, ExtraData, TabBarData, TabAccessor
+
+
+MAGNIFYING_GLASS_ICON="\uf848"
+LEFT_SEP, RIGHT_SEP = ('', '')
+
+HOME = os.path.expanduser('~')
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData
+) -> int:
+    orig_fg = screen.cursor.fg
+    orig_bg = screen.cursor.bg
+
+    def draw_sep(which: str) -> None:
+        screen.cursor.bg = draw_data.default_bg
+        screen.cursor.fg = orig_bg
+        screen.draw(which)
+        screen.cursor.bg = orig_bg
+        screen.cursor.fg = orig_fg
+
+    if max_title_length <= 1:
+        screen.draw('…')
+    elif max_title_length == 2:
+        screen.draw('…|')
+    elif max_title_length < 6:
+        draw_sep(LEFT_SEP)
+        screen.draw((' ' if max_title_length == 5 else '') + '…' + (' ' if max_title_length >= 4 else ''))
+        draw_sep(RIGHT_SEP)
+    else:
+        draw_sep(LEFT_SEP)
+        screen.draw(' ')
+        # draw_title(draw_data, screen, tab, index)
+        ta = TabAccessor(tab.tab_id)
+        screen.draw(os.path.basename(ta.active_wd))
+        if tab.layout_name == 'stack':
+            screen.draw(' ' + MAGNIFYING_GLASS_ICON)
+
+        extra = screen.cursor.x - before - max_title_length
+        if extra >= 0:
+            screen.cursor.x -= extra + 3
+            screen.draw('…')
+        elif extra == -1:
+            screen.cursor.x -= 2
+            screen.draw('…')
+        screen.draw(' ')
+        draw_sep(RIGHT_SEP)
+        draw_sep(' ')
+
+    return screen.cursor.x
