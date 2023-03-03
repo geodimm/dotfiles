494b897c8ed5c2108a12d2680323cf5db0b6bcc7
Georgi Dimitrov <georgijd92@gmail.com>
2023-03-03 17:46:52 +0000

    neovim: fix git-messenger configuration

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,16 +2,16 @@ return {
   { 'tpope/vim-fugitive' },
   {
     'rhysd/git-messenger.vim',
-    config = function()
+    init = function()
       vim.g.git_messenger_include_diff = 'current'
       vim.g.git_messenger_floating_win_opts = { border = 'rounded' }
       vim.g.git_messenger_always_into_popup = true
       vim.g.git_messenger_no_default_mappings = true
       vim.g.git_messenger_popup_content_margins = false
-
-      local keymap = require('utils.keymap')
-      keymap.set('n', '<leader>hh', '<cmd>GitMessenger<CR>', { desc = 'Show history' })
     end,
+    keys = {
+      { '<leader>hh', '<cmd>GitMessenger<CR>', desc = 'Show history' },
+    },
   },
   {
     'lewis6991/gitsigns.nvim',
