1e3daab457498c62052e8826fcb5cb6f8691de2e
Georgi Dimitrov <georgijd92@gmail.com>
2023-03-06 00:36:59 +0000

    neovim: add noice.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -6,6 +6,55 @@ return {
   },
   { 'goolord/alpha-nvim' },
   {
+    'folke/noice.nvim',
+    opts = {
+      lsp = {
+        progress = {
+          enabled = false,
+        },
+        hover = {
+          enabled = false,
+        },
+        signature = {
+          enabled = false,
+        },
+        message = {
+          enabled = false,
+        },
+        override = {
+          -- override the default lsp markdown formatter with Noice
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = false,
+          -- override the lsp markdown formatter with Noice
+          ['vim.lsp.util.stylize_markdown'] = false,
+          -- override cmp documentation with Noice (needs the other options to work)
+          ['cmp.entry.get_documentation'] = false,
+        },
+      },
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+      routes = {
+        -- ignore 'written' messages
+        {
+          filter = {
+            event = 'msg_show',
+            kind = '',
+            find = 'written',
+          },
+          opts = { skip = true },
+        },
+      },
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
     'stevearc/dressing.nvim',
     dependencies = {
       'nvim-telescope/telescope.nvim',
