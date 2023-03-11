ce4b47f8cf90e641bf1bcb3a8f1629f7afd4f0e4
Georgi Dimitrov <georgijd92@gmail.com>
2023-03-11 23:36:08 +0000

    neovim: simplify dressing configuration
    Don't use it for spellsuggest entries as which-key provides a nice UI
    too

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -65,91 +65,29 @@ return {
     dependencies = {
       'nvim-telescope/telescope.nvim',
     },
-    config = function(_, opts)
-      local dressing = require('dressing')
-
-      -- adopted from https://github.com/stevearc/dressing.nvim/blob/12b808a6867e8c38015488ad6cee4e3d58174182/lua/dressing/select/telescope.lua#L8
-      local function indexed_selection(options, defaults, items)
-        local entry_display = require('telescope.pickers.entry_display')
-        local finders = require('telescope.finders')
-        local displayer
-
-        local function make_display(entry)
-          local columns = {
-            { entry.idx .. ':', 'TelescopePromptPrefix' },
-            entry.text,
-            { entry.kind, 'Comment' },
-          }
-          return displayer(columns)
-        end
-
-        local entries = {}
-        local kind_width = 1
-        local text_width = 1
-        local idx_width = 1
-        for idx, item in ipairs(items) do
-          local text = options.format_item(item)
-          local kind = options.kind
-
-          kind_width = math.max(kind_width, vim.api.nvim_strwidth(kind))
-          text_width = math.max(text_width, vim.api.nvim_strwidth(text))
-          idx_width = math.max(idx_width, vim.api.nvim_strwidth(tostring(idx)))
-
-          table.insert(entries, {
-            idx = idx,
-            display = make_display,
-            text = text,
-            kind = kind,
-            ordinal = idx .. ' ' .. text .. ' ' .. kind,
-            value = item,
-          })
-        end
-        displayer = entry_display.create({
-          separator = ' ',
-          items = {
-            { width = idx_width + 1 },
-            { width = text_width },
-            { width = kind_width },
-          },
-        })
-
-        defaults.finder = finders.new_table({
-          results = entries,
-          entry_maker = function(item)
-            return item
-          end,
-        })
-      end
-
-      -- customise the telescope entry style for spellsuggest selections
-      require('dressing.select.telescope').custom_kind['spellsuggest'] = indexed_selection
-
-      opts = vim.tbl_deep_extend('force', opts, {
-        input = {
-          win_options = {
-            winblend = 0,
-          },
-          insert_only = false,
-          prompt_align = 'center',
-          relative = 'editor',
-          prefer_width = 0.5,
+    opts = {
+      input = {
+        win_options = {
+          winblend = 0,
         },
-        select = {
-          telescope = require('telescope.themes').get_cursor({
-            layout_config = {
-              width = function(_, max_columns, _)
-                return math.min(max_columns, 80)
-              end,
-              height = function(_, _, max_lines)
-                return math.min(max_lines, 15)
-              end,
-            },
-          }),
-        },
-      })
-
-      dressing.setup(opts)
-    end,
+        insert_only = false,
+        prompt_align = 'center',
+        relative = 'editor',
+        prefer_width = 0.5,
+      },
+      select = {
+        telescope = require('telescope.themes').get_cursor({
+          layout_config = {
+            width = function(_, max_columns, _)
+              return math.min(max_columns, 80)
+            end,
+            height = function(_, _, max_lines)
+              return math.min(max_lines, 15)
+            end,
+          },
+        }),
+      },
+    },
   },
   {
     'rcarriga/nvim-notify',
