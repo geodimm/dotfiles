5d95c51ebbe7c08ba09a180de0e691943520c3f7
Georgi Dimitrov <georgijd92@gmail.com>
2024-02-11 14:40:25 +0000

    p10k: remove the vpn segment

--- zsh/p10k/segments/vpn_ip.zsh
+++
@@ -1,5 +0,0 @@
-typeset -g POWERLEVEL9K_VPN_IP_FOREGROUND=$green
-typeset -g POWERLEVEL9K_VPN_IP_BACKGROUND=$black
-typeset -g POWERLEVEL9K_VPN_IP_CONTENT_EXPANSION=
-typeset -g POWERLEVEL9K_VPN_IP_INTERFACE='(gpd|wg|(.*tun)|tailscale)[0-9]*'
-typeset -g POWERLEVEL9K_VPN_IP_SHOW_ALL=false
