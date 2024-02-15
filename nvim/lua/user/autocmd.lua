cd2f181d197d348d97374ba62e39166867ef1648
Georgi Dimitrov <georgijd92@gmail.com>
2024-02-15 19:02:36 +0000

    neovim: set ft to helm for yaml files in certain locations

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -27,4 +27,15 @@ vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
   end,
 })
 
+-- Set filetype to helm for YAML files in certain locations {{{1
+vim.api.nvim_create_augroup('user_filetype_helm', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  group = 'user_filetype_helm',
+  desc = 'set filetype to helm for YAML files in certain locations',
+  pattern = { '*/templates/*.yaml', '*/templates/*.tpl' },
+  callback = function()
+    vim.opt_local.filetype = 'helm'
+  end,
+})
+
 --- }}}
