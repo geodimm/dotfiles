20891ac03f37f13f144f7d97f584f169ec67be26
Georgi Dimitrov <georgijd92@gmail.com>
2024-03-03 11:17:06 +0000

    neovim: simplify autocmd groups

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -1,26 +1,18 @@
 -- vim: foldmethod=marker
 
 -- Toggle highlighting current line only in active splits {{{1
-vim.api.nvim_create_augroup('user_toggle_cursorline', { clear = true })
-vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter', 'BufWinEnter' }, {
-  group = 'user_toggle_cursorline',
-  desc = 'enable cursorline on focus',
+vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter', 'BufWinEnter', 'VimLeave', 'WinLeave', 'BufWinLeave' }, {
+  group = vim.api.nvim_create_augroup('user_toggle_cursorline', { clear = true }),
+  desc = 'toggle cursorline on focus',
   pattern = '*',
-  callback = function()
-    vim.opt_local.cursorline = true
-  end,
-})
-vim.api.nvim_create_autocmd({ 'VimLeave', 'WinLeave', 'BufWinLeave' }, {
-  group = 'user_toggle_cursorline',
-  desc = 'disable cursorline on lost focus',
-  pattern = '*',
-  callback = function()
-    vim.opt_local.cursorline = false
+  callback = function(ev)
+    vim.opt_local.cursorline = ev.event:find('Enter') ~= nil
   end,
 })
 
 -- Highlight yanked text {{{1
 vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
+  group = vim.api.nvim_create_augroup('user_highlight_yank', { clear = true }),
   desc = 'highlight yanked text',
   callback = function()
     vim.highlight.on_yank({ higroup = 'Visual', timeout = 500 })
@@ -28,9 +20,8 @@ vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
 })
 
 -- Set filetype to helm for YAML files in certain locations {{{1
-vim.api.nvim_create_augroup('user_filetype_helm', { clear = true })
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-  group = 'user_filetype_helm',
+  group = vim.api.nvim_create_augroup('user_filetype_helm', { clear = true }),
   desc = 'set filetype to helm for YAML files in certain locations',
   pattern = { '*/templates/*.yaml', '*/templates/*.tpl' },
   callback = function()
