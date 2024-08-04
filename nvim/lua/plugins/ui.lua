68e431d01c982a8b049d25f9cc8e9d1bc2a22df8
Georgi Dimitrov <georgijd92@gmail.com>
2024-08-04 15:02:27 +0100

    neovim: update which-key settings

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -126,19 +126,16 @@ return {
     'folke/which-key.nvim',
     opts = function()
       local icons = require('user.icons')
-      local side_margin = math.floor(vim.fn.winwidth(0) / 10)
       return {
         icons = {
           group = icons.ui.list_ul .. ' ',
           breadcrumb = icons.ui.breadcrumb,
         },
-        window = {
+        win = {
+          col = 20,
+          width = vim.fn.winwidth(0) - 40,
           border = 'rounded',
-          margin = { 2, side_margin, 2, side_margin },
           padding = { 1, 1, 1, 1 },
-        },
-        layout = {
-          align = 'center',
         },
       }
     end,
