e2e48c1eb8d736d5910e9c374857ee5aabc5d45e
Georgi Dimitrov <georgijd92@gmail.com>
2024-12-11 16:49:23 +0200

    neovim: update catppuccin integrations

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -10,10 +10,10 @@ return {
       integrations = {
         lsp_trouble = true,
         mason = true,
-        neotest = true,
         noice = true,
         notify = true,
         which_key = true,
+        grug_far = true,
       },
       custom_highlights = function(colors)
         return {
