53bccf4cdb3aaca87eb84a0893954193743a4ff5
Georgi Dimitrov <georgijd92@gmail.com>
2024-12-12 17:09:48 +0200

    p10k: remove f3 segment

--- zsh/p10k/segments/_prompt.zsh
+++ zsh/p10k/segments/_prompt.zsh
@@ -3,7 +3,6 @@
 typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
   # line 1 {{{2
   context                 # user@hostname
-  form3_shell             # f3 shell indicator
   vim_shell               # vim shell indicator (:sh)
   dir                     # current directory
   vcs                     # git status
