a6275bcd08061f1f9e9166b36ec9a1b409759680
Georgi Dimitrov <georgijd92@gmail.com>
2025-01-18 17:42:33 +0200

    neovim: remove git-messenger.vim plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,19 +2,6 @@ return {
   { 'tpope/vim-fugitive' },
   { 'sindrets/diffview.nvim' },
   {
-    'rhysd/git-messenger.vim',
-    init = function()
-      vim.g.git_messenger_include_diff = 'current'
-      vim.g.git_messenger_floating_win_opts = { border = 'rounded' }
-      vim.g.git_messenger_always_into_popup = true
-      vim.g.git_messenger_no_default_mappings = true
-      vim.g.git_messenger_popup_content_margins = false
-    end,
-    keys = {
-      { '<leader>hh', vim.cmd.GitMessenger, desc = 'Show history' },
-    },
-  },
-  {
     'lewis6991/gitsigns.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     init = function()
