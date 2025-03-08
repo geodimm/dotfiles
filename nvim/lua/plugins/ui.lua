72762a448d698fd412e9d39415098e20e8824864
Georgi Dimitrov <georgijd92@gmail.com>
2025-03-08 10:39:13 +0200

    neovim: remove markdown-preview plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -227,4 +227,13 @@ return {
       },
     },
   },
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
 }
