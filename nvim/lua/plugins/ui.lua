8fe259e57b664e7ee0861756e36e5cbc163cd9bf
Georgi Dimitrov <georgijd92@gmail.com>
2025-03-11 09:26:00 +0200

    neovim: move the markview.nvim plugin config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -246,13 +246,4 @@ return {
       },
     },
   },
-  {
-    'OXY2DEV/markview.nvim',
-    lazy = false,
-
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons',
-    },
-  },
 }
