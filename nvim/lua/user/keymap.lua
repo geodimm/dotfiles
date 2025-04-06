3b14a3687683cdb27c2a643d060f0efe1a0b265f
Georgi Dimitrov <georgijd92@gmail.com>
2025-04-06 11:28:04 +0300

    neovim: add a keymap to toggle a floating terminal

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -28,3 +28,43 @@ keymap.set('n', 'ZX', ':qa<CR>', { desc = 'Quitall' })
 -- Abbreviations
 vim.api.nvim_command('iab cdate <c-r>=strftime("%Y-%m-%d")<CR>')
 vim.api.nvim_command('iab todo <c-r>="TODO (Georgi Dimitrov):"<CR>')
+
+-- Toggle terminal
+vim.keymap.set(
+  { 'n', 't' },
+  '<leader>tt',
+  (function()
+    local buf, win = nil, nil
+    local was_insert = true
+    local cfg = function()
+      return {
+        relative = 'editor',
+        width = math.floor(vim.o.columns * 0.8),
+        height = math.floor(vim.o.lines * 0.8),
+        row = math.floor(vim.o.lines * 0.1),
+        col = math.floor(vim.o.columns * 0.1),
+        style = 'minimal',
+        border = 'rounded',
+      }
+    end
+    return function()
+      buf = (buf and vim.api.nvim_buf_is_valid(buf)) and buf or nil
+      win = (win and vim.api.nvim_win_is_valid(win)) and win or nil
+      if not buf and not win then
+        vim.cmd('split | terminal')
+        buf = vim.api.nvim_get_current_buf()
+        vim.api.nvim_win_close(vim.api.nvim_get_current_win(), true)
+        win = vim.api.nvim_open_win(buf, true, cfg())
+      elseif not win and buf then
+        win = vim.api.nvim_open_win(buf, true, cfg())
+      elseif win then
+        was_insert = vim.api.nvim_get_mode().mode == 't'
+        return vim.api.nvim_win_close(win, true)
+      end
+      if was_insert then
+        vim.cmd('startinsert')
+      end
+    end
+  end)(),
+  { desc = 'Floating terminal' }
+)
