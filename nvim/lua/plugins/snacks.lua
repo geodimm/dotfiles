1b863da37fae432e27050606458c46a54a7cd2cb
Georgi Dimitrov <georgijd92@gmail.com>
2025-04-06 15:18:50 +0300

    neovim: move snacks to its own file

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,98 @@
+return {
+  {
+    'folke/snacks.nvim',
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.b.miniindentscope_disable = true
+    end,
+    config = function()
+      local icons = require('user.icons')
+      local opts = {
+        bigfile = {
+          enabled = true,
+        },
+        dashboard = {
+          sections = {
+            { section = 'header' },
+            { section = 'keys', icon = icons.ui.keyboard, indent = 2, padding = 1 },
+            {
+              section = 'recent_files',
+              title = 'Recent Project Files',
+              icon = icons.ui.history,
+              file = vim.fn.fnamemodify('.', ':~'),
+              limit = 5,
+              cwd = true,
+              indent = 2,
+              padding = 1,
+            },
+            {
+              section = 'recent_files',
+              title = 'Recent Files',
+              icon = icons.ui.history,
+              file = vim.fn.fnamemodify('.', ':~'),
+              limit = 5,
+              cwd = vim.g.user_repos_dir or vim.fn.expand('$HOME/repos'),
+              indent = 2,
+              padding = 1,
+            },
+            {
+              section = 'projects',
+              title = 'Projects',
+              icon = icons.file.directory_open,
+              limit = 5,
+              indent = 2,
+              padding = 1,
+            },
+            { section = 'startup', icon = icons.ui.plug .. ' ' },
+          },
+        },
+        gitbrowse = {
+          enabled = true,
+        },
+        image = {
+          enabled = true,
+        },
+        input = {
+          icon_pos = 'title',
+        },
+        styles = {
+          input = {
+            relative = 'cursor',
+            row = -3,
+            col = 0,
+            wo = {
+              winhighlight = 'SnacksInputTitle:Title,SnacksInputIcon:Title',
+            },
+          },
+        },
+      }
+
+      local keymap = require('utils.keymap')
+      keymap.set({ 'n', 'v' }, '<leader>hB', Snacks.gitbrowse.open, { desc = 'Open in browser' })
+      keymap.set('n', '<leader>cR', Snacks.rename.rename_file, { desc = 'Rename File' })
+
+      keymap.register_group('<leader>t', 'Toggle')
+
+      Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>tl')
+      Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>tw')
+      Snacks.toggle.diagnostics():map('<leader>td')
+      Snacks.toggle.inlay_hints():map('<leader>th')
+
+      local feat = require('utils.feat')
+      local formattingToggle = Snacks.toggle.new({
+        id = 'formatting',
+        name = 'Formatting',
+        get = function()
+          return feat.Formatting:is_enabled(0)
+        end,
+        set = function(state)
+          return feat.Formatting:set(0, state)
+        end,
+      }, opts)
+      formattingToggle:map('<leader>tf')
+
+      Snacks.setup(opts)
+    end,
+  },
+}
