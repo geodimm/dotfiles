bf2aad1ad6eed65ea85eb97b08adafe483ca0312
Georgi Dimitrov <georgijd92@gmail.com>
2025-04-08 10:11:48 +0300

    neovim: use lsp foldexpr if available

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -224,6 +224,11 @@ local function on_attach(client, bufnr)
       vim.lsp.buf_detach_client(bufnr, client.id)
     end)
   end
+
+  if client:supports_method('textDocument/foldingRange') then
+    local win = vim.api.nvim_get_current_win()
+    vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+  end
 end
 
 -- Create config that sets up LSP keymaps, augroups and capabilities
