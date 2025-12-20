45937571438ca2360e152183dbf5b7ca1a6a830c
Georgi Dimitrov <georgijd92@gmail.com>
2025-12-20 13:11:15 +0000

    neovim: switch to nvim-treesitter main branch

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,113 +1,187 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
     build = ':TSUpdate',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    opts = {
-      ensure_installed = {
-        'bash',
-        'css',
-        'dockerfile',
-        'git_config',
-        'go',
-        'gomod',
-        'gosum',
-        'gotmpl',
-        'gowork',
-        'hcl',
-        'helm',
-        'html',
-        'http',
-        'java',
-        'javascript',
-        'json',
-        'json5',
-        'jsonc',
-        'jq',
-        'lua',
-        'make',
-        'markdown',
-        'markdown_inline',
-        'pem',
-        'python',
-        'regex',
-        'ruby',
-        'rust',
-        'scss',
-        'starlark',
-        'ssh_config',
-        'terraform',
-        'toml',
-        'typescript',
-        'vim',
-        'vimdoc',
-        'xml',
-        'yaml',
-      },
-      sync_install = false,
-      auto_install = false,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
-          scope_incremental = '<nop>',
-          node_decremental = '<bs>',
-        },
-      },
-      indent = {
-        enable = true,
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
+    config = function()
+      local ts = require('nvim-treesitter')
+
+      -- State tracking for async parser loading
+      local parsers_loaded = {}
+      local parsers_pending = {}
+      local parsers_failed = {}
+
+      local ns = vim.api.nvim_create_namespace('treesitter.async')
+
+      -- Helper to start highlighting and indentation
+      local function start(buf, lang)
+        local ok = pcall(vim.treesitter.start, buf, lang)
+        if ok then
+          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+        return ok
+      end
+
+      -- Install core parsers after lazy.nvim finishes loading all plugins
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'LazyDone',
+        once = true,
+        callback = function()
+          ts.install({
+            'bash',
+            'css',
+            'dockerfile',
+            'git_config',
+            'gitcommit',
+            'go',
+            'gomod',
+            'gosum',
+            'gotmpl',
+            'gowork',
+            'hcl',
+            'helm',
+            'html',
+            'http',
+            'java',
+            'javascript',
+            'json',
+            'json5',
+            'jsonc',
+            'jq',
+            'lua',
+            'make',
+            'markdown',
+            'markdown_inline',
+            'pem',
+            'python',
+            'regex',
+            'ruby',
+            'rust',
+            'scss',
+            'starlark',
+            'ssh_config',
+            'terraform',
+            'toml',
+            'typescript',
+            'vim',
+            'vimdoc',
+            'xml',
+            'yaml',
+          }, {
+            max_jobs = 8,
+          })
+        end,
+      })
+
+      -- Decoration provider for async parser loading
+      vim.api.nvim_set_decoration_provider(ns, {
+        on_start = vim.schedule_wrap(function()
+          if #parsers_pending == 0 then
+            return false
+          end
+          for _, data in ipairs(parsers_pending) do
+            if vim.api.nvim_buf_is_valid(data.buf) then
+              if start(data.buf, data.lang) then
+                parsers_loaded[data.lang] = true
+              else
+                parsers_failed[data.lang] = true
+              end
+            end
+          end
+          parsers_pending = {}
+        end),
+      })
+
+      local group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true })
+
+      -- Auto-install parsers and enable highlighting on FileType
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        desc = 'Enable treesitter highlighting and indentation (non-blocking)',
+        callback = function(event)
+          local lang = vim.treesitter.language.get_lang(event.match) or event.match
+          local buf = event.buf
+
+          if parsers_failed[lang] then
+            return
+          end
+
+          if parsers_loaded[lang] then
+            -- Parser already loaded, start immediately (fast path)
+            start(buf, lang)
+          else
+            -- Queue for async loading
+            table.insert(parsers_pending, { buf = buf, lang = lang })
+          end
+
+          -- Auto-install missing parsers (async, no-op if already installed)
+          ts.install({ lang })
+        end,
+      })
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+    end,
+    opts = {},
+    config = function()
+      local select = require('nvim-treesitter-textobjects.select')
+      local swap = require('nvim-treesitter-textobjects.swap')
+      local move = require('nvim-treesitter-textobjects.move')
+
+      local keymap = require('utils.keymap')
+
+      keymap.set({ 'x', 'o' }, 'af', function()
+        select.select_textobject('@function.outer', 'textobjects')
+      end)
+      keymap.set({ 'x', 'o' }, 'if', function()
+        select.select_textobject('@function.inner', 'textobjects')
+      end)
+      keymap.set({ 'x', 'o' }, 'ac', function()
+        select.select_textobject('@class.outer', 'textobjects')
+      end)
+      keymap.set({ 'x', 'o' }, 'ic', function()
+        select.select_textobject('@class.inner', 'textobjects')
+      end)
+
+      keymap.set({ 'n', 'x', 'o' }, ']m', function()
+        move.goto_next_start('@function.outer', 'textobjects')
+      end, { desc = 'Next function start' })
+      keymap.set({ 'n', 'x', 'o' }, ']M', function()
+        move.goto_next_end('@function.outer', 'textobjects')
+      end, { desc = 'Next function end' })
+      keymap.set({ 'n', 'x', 'o' }, '[m', function()
+        move.goto_previous_start('@function.outer', 'textobjects')
+      end, { desc = 'Previous function start' })
+      keymap.set({ 'n', 'x', 'o' }, '[M', function()
+        move.goto_previous_end('@function.outer', 'textobjects')
+      end, { desc = 'Previous function end' })
+
+      keymap.set({ 'n', 'x', 'o' }, ']]', function()
+        move.goto_next_start('@class.outer', 'textobjects')
+      end, { desc = 'Next class start' })
+      keymap.set({ 'n', 'x', 'o' }, '][', function()
+        move.goto_next_end('@class.outer', 'textobjects')
+      end, { desc = 'Next class end' })
+      keymap.set({ 'n', 'x', 'o' }, '[[', function()
+        move.goto_previous_start('@class.outer', 'textobjects')
+      end, { desc = 'Previous class start' })
+      keymap.set({ 'n', 'x', 'o' }, '[]', function()
+        move.goto_previous_end('@class.outer', 'textobjects')
+      end, { desc = 'Previous class end' })
+
+      keymap.set('n', '<leader>a', function()
+        swap.swap_next('@parameter.inner')
+      end, { desc = 'Swap next parameter' })
+      keymap.set('n', '<leader>A', function()
+        swap.swap_previous('@parameter.inner')
+      end, { desc = 'Swap previous parameter' })
     end,
   },
   {
