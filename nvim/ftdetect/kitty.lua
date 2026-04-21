bdb7245dff6ee797de64b492625f3459653f2fc8
Georgi Dimitrov <georgijd92@gmail.com>
2026-04-21 11:37:11 +0300

    neovim: fix ftdetect

--- nvim/ftdetect/kitty.lua
+++ nvim/ftdetect/kitty.lua
@@ -1,5 +1,5 @@
 vim.filetype.add({
   pattern = {
-    ['.*/kitty.*.conf'] = 'kitty',
+    ['.*/kitty.*%.conf'] = 'kitty',
   },
 })
--- nvim/ftdetect/starlark.lua
+++ nvim/ftdetect/starlark.lua
@@ -1,4 +1,8 @@
 vim.filetype.add({
+  filename = {
+    Tiltfile = 'starlark',
+    tiltfile = 'starlark',
+  },
   pattern = {
     ['.*/.*Tiltfile.*'] = 'starlark',
     ['.*/.*tiltfile.*'] = 'starlark',
