aa5430b15c069a09df5f502d22568ea5e3ad504c
Georgi Dimitrov <georgijd92@gmail.com>
2026-04-21 11:52:58 +0300

    neovim: update imports and deps

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'kyazdani42/nvim-tree.lua',
+    'nvim-tree/nvim-tree.lua',
     init = function()
       local prev = { new_name = '', old_name = '' } -- Prevents duplicate events
       vim.api.nvim_create_autocmd('User', {
