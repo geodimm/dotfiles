cc1bb70c01f7a49a8f15e2dd497529f0cc7e1b85
Georgi Dimitrov <georgijd92@gmail.com>
2026-09-06 17:33:11 +0300

    neovim: enable ghostty-smart-splits bridge

--- nvim/lua/plugins/smart-splits.lua
+++ nvim/lua/plugins/smart-splits.lua
@@ -14,7 +14,7 @@ function M.setup()
   keymap.set('n', '<C-\\>', smartsplits.move_cursor_previous, { desc = 'Switch to the last window' })
 
   smartsplits.setup({})
-  require('ghostty-smart-splits').setup()
+  require('ghostty-smart-splits').setup({ bridge = true })
 end
 
 return M
