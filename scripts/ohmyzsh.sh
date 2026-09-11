88489123439baf26caef4adeaf877b5dbacf7ef3
Georgi Dimitrov <georgijd92@gmail.com>
2026-09-11 19:23:45 +0300

    ohmyzsh: bring back F-Sy-H

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -9,7 +9,7 @@ ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
 
 declare -a ZSH_CUSTOM_PLUGINS=(
     "themes/powerlevel10k=https://github.com/romkatv/powerlevel10k"
-    "plugins/zsh-syntax-highlighting=https://github.com/zsh-users/zsh-syntax-highlighting"
+    "plugins/F-Sy-H=https://github.com/z-shell/F-Sy-H"
     "plugins/zsh-autosuggestions=https://github.com/zsh-users/zsh-autosuggestions"
     "plugins/zsh-completions=https://github.com/zsh-users/zsh-completions"
     "plugins/you-should-use=https://github.com/MichaelAquilina/zsh-you-should-use"
